0716ba3cf0543a2c6212cca3eff93c8e0679d28f
Guilherme Utech <utech.btc@gmail.com>
2024-06-24 21:35:21 -0300

    consumer will print the message it gets from the publisher via RabbitMQ

--- Send/Send.cs
+++ Send/Send.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost"};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+const string msg = "Hello world";
+var body = Encoding.UTF8.GetBytes(msg);
+
+channel.BasicPublish(exchange: string.Empty, routingKey: "hello", basicProperties: null, body: body);
+
+Console.WriteLine($" [x] Sent {msg}");
+Console.WriteLine(" Press [enter] to exit.");
+
+Console.ReadLine();
