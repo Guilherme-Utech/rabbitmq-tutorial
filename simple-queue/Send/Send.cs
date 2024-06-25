481448b6b96d9a13cfcd678989f6f87663e35772
Guilherme Utech <utech.btc@gmail.com>
2024-06-25 19:38:20 -0300

    folder change

---
+++ simple-queue/Send/Send.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost"};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "hello"
+                    , durable: false
+                    , exclusive: false
+                    , autoDelete: false
+                    , arguments: null);
+
+while (true)
+{
+    Console.WriteLine("Type a message ... (Type END to close the program)");
+    var msg = Console.ReadLine();
+
+    if (msg == "END")
+        return;
+
+    var body = Encoding.UTF8.GetBytes(msg);
+
+    channel.BasicPublish(exchange: string.Empty
+                        , routingKey: "hello"
+                        , basicProperties: null
+                        , body: body)
+    ;
+}
